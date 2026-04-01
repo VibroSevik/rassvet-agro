6628a29bd3c8f2294135f1dee279e0494d98812d
Vsevolod <ash.seva@gmail.com>
2026-04-01 19:26:49 +0300

    :wheelchair: added ResponseStatuses

---
+++ system/library/responseutils/responsestatuses.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ResponseUtils;
+
+class ResponseStatuses {
+    public const HTTP_METHOD_NOT_ALLOWED = [
+        'text' => 'Method Not Allowed',
+        'code' => 405
+    ];
+}
