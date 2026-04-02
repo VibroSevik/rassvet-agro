255463f095653eddd0a2f5e5bfc9c3a07444dafc
Vsevolod <ash.seva@gmail.com>
2026-04-02 21:43:18 +0300

    :pencil2: added new ResponseStatuses

--- system/library/responseutils/responsestatuses.php
+++ system/library/responseutils/responsestatuses.php
@@ -3,6 +3,16 @@
 namespace ResponseUtils;
 
 class ResponseStatuses {
+    public const HTTP_OK = [
+        'text' => 'OK',
+        'code' => 200
+    ];
+
+    public const HTTP_BAD_REQUEST = [
+        'text' => 'Bad Request',
+        'code' => 400
+    ];
+
     public const HTTP_METHOD_NOT_ALLOWED = [
         'text' => 'Method Not Allowed',
         'code' => 405
