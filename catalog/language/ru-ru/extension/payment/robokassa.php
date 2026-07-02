a73dc25b31b316a352e60c550f70c581c26f24fe
Vsevolod <ash.seva@gmail.com>
2026-07-02 15:27:19 +0300

    :pencil2: changed payment methods titles

--- catalog/language/ru-ru/extension/payment/robokassa.php
+++ catalog/language/ru-ru/extension/payment/robokassa.php
@@ -1,4 +1,4 @@
 <?php
 // Text
-$_['text_title']       = 'Робокасса <img src="image/payment/robokassa.png" alt="" style="max-width: 150px;">';
-$_['text_description']       = 'Робокасса <img src="image/payment/robokassa.png" alt="" style="max-width: 150px;">';
+$_['text_title']       = 'Оплата картой онлайн <img src="image/payment/robokassa.png" alt="" style="max-width: 150px;">';
+$_['text_description']       = 'Оплата картой онлайн <img src="image/payment/robokassa.png" alt="" style="max-width: 150px;">';
